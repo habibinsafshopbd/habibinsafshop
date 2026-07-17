12a2bd28de37d8d1bf30af2bfdd97a7d51e54179
habibinsafshopbd <abdullahhabib2122@gmail.com>
2026-07-17 20:30:24 +0600

    Fix syntax errors in menuOpen function

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function menuOpen() {
     var menu = document.getElementById("menu");
-eflet 
+
     if (menu.style.display === "block") {
         menu.style.display = "none";
     } else {
         menu.style.display = "block";
     }
-}'. ebb.  e g. wggwwdrqefeggggwggv
+}
